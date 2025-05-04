e96e9b19971f5e8a476419cc1f8a207d7c4a6192
Mehran <103825673+AetherMaker@users.noreply.github.com>
2025-05-04 08:53:52 +0300

    Refactor: Remove disk saves for generated images in app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cf5bd8af3ecf73ba69f92ebfd6fd78081d48a14068c8d28fe7ae78630704dc5
-size 10726
+oid sha256:dea1b3b014b477076d2f6e35719a6bb308df9a05eb0e9ff168be4aad72e142e7
+size 10733
